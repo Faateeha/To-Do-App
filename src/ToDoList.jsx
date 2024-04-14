33f62077895d1c3f2e1ab75cc3cb9ec69a98494e
Adeyemi Fathia <fathiaomolara02@gmail.com>
2024-04-14 07:17:26 +0100

    initial commit

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 function ToDoList() {
     
-    const [tasks, setTasks] = useState(["Eat", "Sleep", "Code"]);
+    const [tasks, setTasks] = useState([]);
     const [newTask, setNewTask] = useState("");
 
     function handleInput(event){
